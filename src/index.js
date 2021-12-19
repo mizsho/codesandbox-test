bb69bf88127d77894add25de66133bbaba415d4b
mizsho <34414292+mizsho@users.noreply.github.com>
2021-12-19 21:19:09 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-import "./styles.css";
+/**
+ * 三項演算子
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています!!" : "許容範囲内です";
+// };
+
+// console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう
+ */
+
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+
+// || は左側がfalseなら右側を返す
+// const num = null;
+// // const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+const num2 = 100;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
